96538c35934b0fa50debe4a4762762fb39b05de8
Juan Carlos <charliejuc@gmail.com>
2024-11-03 23:50:51 +0100

    Supports different datatypes for index

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ import { faker } from "@faker-js/faker";
 
 // It is and example of the behavior of a simple database index to show the performance issues
 
-const categoriesIds = Array.from({ length: 20 }, () => faker.string.uuid());
+const categoriesLength = 5;
+const transactionsLength = 20;
+const daysRange = 5;
 
+const categoriesIds = Array.from({ length: categoriesLength }, () =>
+  faker.string.uuid()
+);
+const sumDays = (date, offset) => {
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + offset);
+  return newDate;
+};
 const generateTransaction = () => {
   const randomDate = () =>
     faker.date.between({
-      from: new Date("2021-01-01"),
+      from: sumDays(new Date(), daysRange * -1),
       to: new Date(),
     });
 
@@ -16,8 +26,7 @@ const generateTransaction = () => {
     amount: Number(
       faker.finance.amount({
         min: 0,
-        max: 10,
-        dec: 0,
+        max: 5,
       })
     ),
     date: randomDate(),
@@ -28,14 +37,32 @@ const generateTransaction = () => {
   };
 };
 
-const transactions = Array.from({ length: 30 }, generateTransaction);
+const transactions = Array.from(
+  { length: transactionsLength },
+  generateTransaction
+);
 
+const parseValueByType = (value) => {
+  if (typeof value === "string") {
+    return value;
+  }
+
+  if (typeof value === "number") {
+    return Math.floor(value);
+  }
+
+  if (value instanceof Date) {
+    return value.toISOString().split("T")[0];
+  }
+
+  return JSON.stringify(value);
+};
 const createIndex = (documents, fields, _index = {}) => {
   const index = _index ?? {};
   const field = fields[0];
 
   for (const document of documents) {
-    const value = document[field];
+    const value = parseValueByType(document[field]);
 
     if (fields.length > 1) {
       index[value] = createIndex([document], fields.slice(1), index[value]);
@@ -53,5 +80,9 @@ const createIndex = (documents, fields, _index = {}) => {
 };
 
 console.log(
-  JSON.stringify(createIndex(transactions, ["categoryId", "amount"]), null, 2)
+  JSON.stringify(
+    createIndex(transactions, ["categoryId", "amount", "date"]),
+    null,
+    2
+  )
 );
